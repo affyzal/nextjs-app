09dcf96871a5b6d379f2726e3339450d9135aa54
Afzal Miah <affyzal@gmail.com>
2025-11-05 01:46:45 +0000

    font

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -42,7 +42,7 @@ export default function Contact({
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <motion.h2
             variants={item}
-            className="max-w-xs text-3xl font-semibold leading-10 tracking-tight"
+            className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-zinc-50"
           >
             <span className="text-[#64ffda]">04. </span>Let&apos;s Build
             Something Together
